0d2919682b15a475ed9c3d228c297714d30e0302
TikaThamsuhang <tikathamsuhang1@gmail.com>
2024-08-27 22:56:18 +0545

    Update form.py

--- todo/form.py
+++ todo/form.py
@@ -4,4 +4,4 @@ from .models import todo
 class TodoForm(forms.ModelForm):
     class Meta:
         model = todo
-        fields = ['todo_name']  # Include only the fields you want to edit
+        fields = ['todo_name'] 
